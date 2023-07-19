21da81ea45e6ee590d3fc27d16f06d1a1451bcda
Breno Pinna <brenopinna1008@gmail.com>
2023-07-19 16:18:07 -0300

    :sparkles: feat: add first route

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("<h1>Minha lista de tarefas</h1>")
+})
+
+app.listen(3000, () => {
+  console.clear()
+  console.log("🚀 HTTP Server Running at localhost:3000")
+})
